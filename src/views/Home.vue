0209aaf392f587c3e32386677dddf20b8c6a5329
Ian Boyle <irboyle21@gmail.com>
2021-11-17 11:31:23 -0500

    adds create method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,25 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h2>{{ message }}</h2>
+    <div v-for="place in places" :key="place.id">
+      <h3>{{ place.name }}</h3>
+      <p>{{ place.address }}</p>
+    </div>
+    <h3>Create a new place!</h3>
+    <form v-on:submit.prevent="createPlace()">
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Name:</label>
+        <input type="text" v-model="newPlaceParams.name" />
+      </div>
+      <div>
+        <label>Address:</label>
+        <input type="text" v-model="newPlaceParams.address" />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
   </div>
 </template>
 
@@ -13,6 +32,8 @@ export default {
     return {
       message: "Welcome to my Places App.",
       places: [],
+      newPlaceParams: {},
+      errors: [],
     };
   },
   created: function () {
@@ -24,6 +45,15 @@ export default {
         this.places = response.data;
         console.log("Success!", response.data);
       });
+    },
+    createPlace: function () {
+      axios
+        .post("http://localhost:3000/places", this.newPlaceParams)
+        .then(() => {
+          this.$router.push("/places");
+          console.log("Success!");
+        })
+        .catch((error) => console.log(error.response));
     },
   },
 };
